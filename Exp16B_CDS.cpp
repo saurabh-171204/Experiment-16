c111c6c3b377a69650d3054a60ae2c3cf4a31ff6
saurabh-171204 <saurabhbihani5023@gmail.com>
2024-10-08 19:17:39 +0530

    Add files via upload

---
+++ Exp16B_CDS.cpp
@@ -0,0 +1,36 @@
+// NAME - SAURABH BIHANI
+// PRN - 23070123166
+// EXPERIMENT - 16(B) 
+
+// EXCEPTION HANDLING 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18) {
+            throw age;
+        }
+        else {
+            cout<<"Age: "<<age<<"\n APPROVED"<<endl;
+        }
+    }
+    catch(int a) {
+        cout<<"\n ERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
+
+// Output 
+
+// Enter age: 7 
+//  
+//  ERROR: Underage! (7) 
+
+// Enter age: 20
+// Age: 20
+//  APPROVED 
